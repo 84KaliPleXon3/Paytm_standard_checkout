958784ec3cf37ca845d9e99f6ac628009e8276e9
Navjot singh <ns949405@gmail.com>
2020-05-07 15:28:19 +0530

    Update pgRedirect.php

--- pgRedirect.php
+++ pgRedirect.php
@@ -23,7 +23,7 @@ $paramList["INDUSTRY_TYPE_ID"] = $INDUSTRY_TYPE_ID;
 $paramList["CHANNEL_ID"] = $CHANNEL_ID;
 $paramList["TXN_AMOUNT"] = $TXN_AMOUNT;
 $paramList["WEBSITE"] = PAYTM_MERCHANT_WEBSITE;
-$paramList["CALLBACK_URL"] = "http://localhost/paytm/callback.php";
+$paramList["CALLBACK_URL"] = "http://localhost/Paytm_standard_checkout/callback.php";
 /*
 $paramList["MSISDN"] = $MSISDN; //Mobile number of customer
 $paramList["EMAIL"] = $EMAIL; //Email ID of customer
