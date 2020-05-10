d159af8ba8a615c935ef4cca5c5c5991974276b8
Navjot singh <ns949405@gmail.com>
2020-05-10 16:19:21 +0530

    Update nav.php

--- include/nav.php
+++ include/nav.php
@@ -5,7 +5,7 @@
              Payment Intigration 
           </a>
            <ul class="nav navbar-nav navbar-right">
-           	  <li> <a href="#"> <strong><i class="fab fa-youtube text-danger" ></i> Video Demo</strong></a></li>
+           	  <li> <a href="https://youtu.be/gvpShOeYd5k"> <strong><i class="fab fa-youtube text-danger" ></i> Video Demo</strong></a></li>
 		      <li> <a href="https://www.youtube.com/channel/UCVlSbZdK_7tTF_X91gpD48g?view_as=subscriber"> <strong><i class="fab fa-youtube text-danger" ></i> Codelone</strong></a></li>
 			  <li><a href="https://instagram.com/code_lone/"> <strong><i class="fab fa-instagram text-default" ></i> instagram.com</a></li>
 		    </ul>
